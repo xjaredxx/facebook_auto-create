a911454e4bafa26ac170c7b80c726a7ecb225955
xjaredxx <j2305695@gmail.com>
2026-03-09 13:15:43 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-# run.py - TAMA NA ITO!
+
 import Jared_auto_createfb
 
-# Tawagin mo yung function na NAGDADALAWA NG MAIN MENU
-Jared_auto_createfb.menu()  # O Jared_auto_createfb.main() / Jared_auto_createfb.start()
+Jared_auto_createfb.main()
